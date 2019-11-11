8e918e1a617df1c5f555f1d444369e3cca281e81
Dugi97 <lazardugalic1@gmail.com>
2019-11-11 21:22:12 +0100

    first commit

---
+++ src/Entity/Movie.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MovieRepository")
+ */
+class Movie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $releaseDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $plot;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $movieLength;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $youtubeLink;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $comment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTimeInterface
+    {
+        return $this->releaseDate;
+    }
+
+    public function setReleaseDate(\DateTimeInterface $releaseDate): self
+    {
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    public function getPlot(): ?string
+    {
+        return $this->plot;
+    }
+
+    public function setPlot(string $plot): self
+    {
+        $this->plot = $plot;
+
+        return $this;
+    }
+
+    public function getMovieLength(): ?string
+    {
+        return $this->movieLength;
+    }
+
+    public function setMovieLength(string $movieLength): self
+    {
+        $this->movieLength = $movieLength;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getYoutubeLink(): ?string
+    {
+        return $this->youtubeLink;
+    }
+
+    public function setYoutubeLink(string $youtubeLink): self
+    {
+        $this->youtubeLink = $youtubeLink;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
---
+++ src/Form/MovieType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('releaseDate', DateType::class)
+            ->add('plot', TextType::class)
+            ->add('movieLength', NumberType::class)
+            ->add('image', TextType::class)
+            ->add('youtubeLink', TextType::class)
+            ->add('comment', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
